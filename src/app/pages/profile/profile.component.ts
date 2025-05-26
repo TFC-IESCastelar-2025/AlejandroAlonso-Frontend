37f09868fdf1cac69c2a87c8ec4175a0dfe73700
Alexhappy64 <aalonsos21@iescastelar.com>
2025-05-26 18:52:17 +0200

    refactor: redesign profile

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -5,6 +5,7 @@ import { AuthService, UserProfile } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
   user: UserProfile | null = null;
